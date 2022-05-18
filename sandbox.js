aad2043ad3b2773057308c63fd941fc026054520
ROMDEVY <jheromelamosa@gmail.com>
2022-05-18 20:02:43 +0800

    edit text

---
+++ sandbox.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function($) {
+
+    
+    // $('.slider-cotainer').slick({
+    //     infinite: false,
+    //     speed: 300,
+    //     slidesToShow: 2,
+    //     slidesToScroll: 1,
+    //     dots: true,
+    //     arrow: true
+    //   });
+
+
+});
+
