843e264dce2e91a98065f3e7526fc0106121684c
ROMDEVY <jheromelamosa@gmail.com>
2022-05-19 09:31:46 +0800

    update

--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,6 @@
 jQuery(document).ready(function($) {
 
     
-// $('.banner-slider-cotainer').slick({
-//     infinite: true,
-//     slidesToShow: 1,
-//     slidesToScroll: 1
-//   });
-
-
-
 
 });
 
