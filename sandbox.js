56c5c709184a5278f1b837be0435918805a17433
ROMDEVY <jheromelamosa@gmail.com>
2022-05-20 11:29:19 +0800

    update

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,10 @@
 jQuery(document).ready(function($) {
 
-    
+        $("#change-season").click(function(){
+            $("#title").toggleClass("summer-background");
+            $("#leaves").toggleClass("display-block");
+            $(".snow-flakes").toggleClass("display-none");
+          });
 
 });
 
